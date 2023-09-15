5e106baf99bc9fd9fd39ff741bf59ce5ac48f815
housenkui <housenkui@gmail.com>
2023-09-15 15:16:56 +0800

    add files

---
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,11 @@
+public struct MyLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func Log() {
+        print("6666666666666666")
+    }
+    
+}
